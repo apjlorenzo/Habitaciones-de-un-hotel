77fea25653877a5eb6c930dd636228fed15df2c7
Pablo Lorenzo <apjlorenzo1@gmail.com>
2024-08-07 09:01:17 -0600

    Cambio 2

---
+++ Habitación.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_02
+{
+    internal class Habitación
+    {
+        public List<Habitación> Habitacion;
+        //Atributos
+        public int Numero { get; set; }
+        public double PrecioPorNoche { get; set; }
+        public bool Disponibilidad { get; set; }
+        public string ClienteAsignado { get; set; }
+        //Método constructor
+        public Habitación(int numero, double precioPorNoche, bool disponibilidad, string clienteAsignado)
+        {
+            Numero = numero;
+            PrecioPorNoche = precioPorNoche;
+            Disponibilidad = disponibilidad;
+            ClienteAsignado = clienteAsignado;
+        }
+        //Métodos
+        public void MostrarInformacion()
+        {
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
-﻿bool menucondition = true;
+﻿
+using Laboratorio_02;
+
+List <Habitación> habitacion = new List<Habitación> ();
+bool menucondition = true;
 do
 {
     Console.WriteLine("______MENU______");
@@ -8,5 +12,39 @@ do
     Console.WriteLine("4.Asignar habitaciones");
     Console.WriteLine("5.Liberar habitaciones");
     Console.WriteLine("6.Salir del programa");
+    int option = int.Parse(Console.ReadLine());
+    try
+    {
+        switch (option)
+        {
+            case 1:
+                Console.Clear();
+                Console.ReadKey();
+                break;
+            case 2:
+                Console.Clear();
+                Console.ReadKey();
+                break;
+            case 3:
+                Console.Clear();
+                Console.ReadKey();
+                break;
+            case 4:
+                Console.Clear();
+                Console.ReadKey();
+                break;
+            case 5:
+                Console.Clear();
+                Console.ReadKey();
+                break;
+            case 6:
+                menucondition = false;
+                break;
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
 }
 while (menucondition);
