3feedd30ba84e7b68ff0a84cee097b30a3d96027
Pablo Lorenzo <apjlorenzo1@gmail.com>
2024-08-07 08:52:50 -0600

    Menu

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool menucondition = true;
+do
+{
+    Console.WriteLine("______MENU______");
+    Console.WriteLine("1.Agregar habitaciones");
+    Console.WriteLine("2.Eliminar habitación");
+    Console.WriteLine("3.Mostrar habitaciones");
+    Console.WriteLine("4.Asignar habitaciones");
+    Console.WriteLine("5.Liberar habitaciones");
+    Console.WriteLine("6.Salir del programa");
+}
+while (menucondition);
